0c15e3aa2517de4e11a65958658f93320ff87f05
unknown <vietnqhe151190@fpt.edu.vn>
2022-02-16 16:19:23 +0700

    create prọect

---
+++ src/java/dal/DBContext.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dal;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Sap-lap
+ */
+public class DBContext {
+    protected Connection connection;
+    public DBContext()
+    {
+        try {
+            String user = "viet ";
+            String pass = "162001";
+            String url = "jdbc:sqlserver://DESKTOP-SOE5BM3;databaseName=SE1616";
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(url, user, pass);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    //code here
+}
